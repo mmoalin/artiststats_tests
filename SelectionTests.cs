4c2153d938d3e7b8202ff8e07696f0e2259a098f
Mo Moalin <mmoalin@outlook.com>
2020-01-06 11:12:35 +0000

    mostly works, prob needs final tests

--- SelectionTests.cs
+++ SelectionTests.cs
@@ -6,6 +6,8 @@ using ArtistStats_web.Helpers;
 using ArtistStats_web.Services;
 using ArtistStats_web.Commands;
 using System.Threading.Tasks;
+using Moq;
+using System;
 /************************************************************************************
 Solution selects the best options regards to an artist or a release:
 - Artists must a) match with the input artist & b) have the most relea ses if duplicates exist for the artist or 
@@ -19,6 +21,132 @@ namespace ArtistStats.Test
 {
     public class SelectionTests
     {
+        public static IEnumerable<object[]> GetDuplicatesTestParams()
+        {
+            string artistName = "Tupac shakur";
+            var mockService = new Mock<IMusicStatService>();
+            var realService = new MusicStatService();
+            Func<object, string> getReleasesFileData = (object path) =>
+            {
+                return TestHelper.GetFileData((string)path);
+            };
+            Func<object, string> getTracksData = (object releaseID) =>
+            {
+                Task<string> tracksTask = realService.getTracks((string)releaseID);
+                tracksTask.Wait();
+                return tracksTask.Result;
+            };
+            Func<object, object, Task<string>> getTracksLyrics = (object artistName, object trackTitle) =>
+            {
+                return realService.getLyrics((string)artistName, (string)trackTitle);
+            };
+
+            mockService.Setup(mss => mss.getArtists(artistName))
+                 .Returns(Task<string>.Factory.StartNew(getReleasesFileData, "data/artists-query-2pac.json"));
+            mockService.Setup(mss => mss.getReleases("92a4d187-168d-4422-8d04-d194bea5da47"))
+                 .Returns(Task<string>.Factory.StartNew(getReleasesFileData, "data/releases-artist-tupac-shakur.json"));
+            mockService.Setup(mss => mss.getReleases("382f1005-e9ab-4684-afd4-0bdae4ee37f2"))
+                 .Returns(Task<string>.Factory.StartNew(getReleasesFileData, "data/releases-artist-2pac.json"));
+
+            mockService.Setup((mss) => mss.getTracks(It.IsAny<string>()))
+                 .Returns<string>((s) => Task<string>.Factory.StartNew(getTracksData, s));
+            mockService.Setup((mss) => mss.getLyrics(It.IsAny<string>(), It.IsAny<string>()))
+                 .Returns<string, string>(async (artistName, trackTitle) => await await Task.Factory.StartNew(() => getTracksLyrics(artistName, trackTitle)));
+            var mock = mockService.Object;
+            var real = new MusicStatService();
+            yield return new object[] { mock, artistName };
+        }
+        public static IEnumerable<object[]> GetTracksAsyncTestParams()
+        {
+            string artistName = "Tupac shakur";
+            var mockService = new Mock<IMusicStatService>();
+            var realService = new MusicStatService();
+            Func<object, string> getReleasesFileData = (object path) =>
+            {
+                return TestHelper.GetFileData((string)path);
+            };
+            Func<object, string> getTracksData = (object releaseID) =>
+            {
+                Task<string> tracksTask = realService.getTracks((string)releaseID);
+                tracksTask.Wait();
+                return tracksTask.Result;
+            };
+            Func<object, object, Task<string>> getTracksLyrics = (object artistName, object trackTitle) =>
+            {
+                return realService.getLyrics((string)artistName, (string)trackTitle);
+            };
+
+            mockService.Setup(mss => mss.getArtists(artistName))
+                 .Returns(Task<string>.Factory.StartNew(getReleasesFileData, "data/artists-query-2pac.json"));
+            mockService.Setup(mss => mss.getReleases("92a4d187-168d-4422-8d04-d194bea5da47"))
+                 .Returns(Task<string>.Factory.StartNew(getReleasesFileData, "data/releases-artist-tupac-shakur.json"));
+            mockService.Setup(mss => mss.getReleases("382f1005-e9ab-4684-afd4-0bdae4ee37f2"))
+                 .Returns(Task<string>.Factory.StartNew(getReleasesFileData, "data/releases-artist-2pac.json"));
+
+            mockService.Setup((mss) => mss.getTracks(It.IsAny<string>()))
+                 .Returns<string>((s) => Task<string>.Factory.StartNew(getTracksData, s));
+            mockService.Setup((mss) => mss.getLyrics(It.IsAny<string>(), It.IsAny<string>()))
+                 .Returns<string, string>(async (artistName, trackTitle) => await await Task.Factory.StartNew(() => getTracksLyrics(artistName, trackTitle)));
+            var mock = mockService.Object;
+            Task<string> artistsResultsTask = mock.getArtists(artistName);
+            artistsResultsTask.Wait();
+            string artistsResultsJson = artistsResultsTask.Result;
+            DeserializeJson<ArtistResults> deserializeJson = new DeserializeJson<ArtistResults>();
+            ArtistResults artistResults = deserializeJson.Deserialize(artistsResultsJson);
+            var real = new MusicStatService();
+            yield return new object[] { mock, artistResults.Artists[1] };
+        }
+        public static IEnumerable<object[]> GetAveraveWordsTestParams()
+        {
+            string artistName = "Tupac shakur";
+            var mockService = new Mock<IMusicStatService>();
+            var realService = new MusicStatService();
+            Func<object, string> getReleasesFileData = (object path) =>
+            {
+                return TestHelper.GetFileData((string)path);
+            };
+            Func<object, string> getTracksData = (object releaseID) =>
+            {
+                Task<string> tracksTask = realService.getTracks((string)releaseID);
+                tracksTask.Wait();
+                return tracksTask.Result;
+            };
+            Func<object, object, Task<string>> getTracksLyrics = (object artistName, object trackTitle) =>
+            {
+                return realService.getLyrics((string)artistName, (string)trackTitle);
+            };
+
+            mockService.Setup(mss => mss.getArtists(artistName))
+                 .Returns(Task<string>.Factory.StartNew(getReleasesFileData, "data/artists-query-2pac.json"));
+            mockService.Setup(mss => mss.getReleases("92a4d187-168d-4422-8d04-d194bea5da47"))
+                 .Returns(Task<string>.Factory.StartNew(getReleasesFileData, "data/releases-artist-tupac-shakur.json"));
+            mockService.Setup(mss => mss.getReleases("382f1005-e9ab-4684-afd4-0bdae4ee37f2"))
+                 .Returns(Task<string>.Factory.StartNew(getReleasesFileData, "data/releases-artist-2pac.json"));
+
+            mockService.Setup((mss) => mss.getTracks(It.IsAny<string>()))
+                 .Returns<string>((s) => Task<string>.Factory.StartNew(getTracksData, s));
+            mockService.Setup((mss) => mss.getLyrics(It.IsAny<string>(), It.IsAny<string>()))
+                 .Returns<string, string>(async (artistName, trackTitle) => await await Task.Factory.StartNew(() => getTracksLyrics(artistName, trackTitle)));
+            var mock = mockService.Object;
+            var real = new MusicStatService();
+            yield return new object[] { mock, artistName };
+            artistName = "Drake";
+            yield return new object[] { real, artistName };
+        }
+        [Theory]
+        [MemberData(nameof(GetAveraveWordsTestParams))]
+        public void getsAndCalculatesAverageWords(IMusicStatService service, string artistName)
+        {
+            MusicInterpretor sut = new MusicInterpretor(service);
+            Artist artist = sut.PickArtist(artistName);
+            var average = sut.calculateArtistStats(artist);
+            Assert.True(average.AverageWords > 0);
+        }
+        [Fact]
+        public void getsStdDev()
+        {
+
+        }
         [Fact]
         public void SelectsValidArtist()
         {
@@ -31,8 +159,39 @@ namespace ArtistStats.Test
             Assert.False(artist == null);
             Assert.False(artist.Releases.Length == 0);
         }
+        [Theory]
+        [MemberData(nameof(GetTracksAsyncTestParams))]
+        public void GetsTrackAsync(IMusicStatService service, Artist artist)
+        {
+            GetReleasesByArtistsIDAsyncFactory getReleasesByArtistsIDAsync = new GetReleasesByArtistsIDAsyncFactory(artist, service);
+            Command getReleasesByArtistsIDAsync1 = getReleasesByArtistsIDAsync.GetCommand();
+            getReleasesByArtistsIDAsync1.ExecuteAsync().Wait();
+            GetReleasesByArtistsIDAsync cmd = (GetReleasesByArtistsIDAsync)getReleasesByArtistsIDAsync1;
+            var tracks = cmd.ReleaseResults.Releases.SelectMany(r => r.Media[0].Tracks).ToList();
+            Assert.True(tracks.Count > 0);
+        }
+        [Theory]
+        [MemberData(nameof(GetDuplicatesTestParams))]
+        public void FiltersDuplicateTracksFromReleases(IMusicStatService service, string artistName)
+        {
+            MusicInterpretor sut = new MusicInterpretor(service);
+            var artist = sut.PickArtist(artistName);
+            var releases = new List<Release>(artist.Releases);
+            List<Track> uniqueTracks = releases.SelectMany(x => x.Media[0].Tracks).ToList();
+            //??
+            
+            for (int i = 0; i < releases.Count; i++)
+            {
+
+            }
+            /*for (int i = 0; i < rawTracks.Count; i++)
+            {
+                Track verification = uniqueTracks.Find(x => x.Title == rawTracks[i].Title);
+                Assert.NotNull(verification);
+            }*/
+        }
         [Fact]
-        public void SelectsValidReleases()
+        public void SelectsValidReleasesWithTracks()
         {
             string fileWithValues = "data/releases-artist-2pac.json";
             var values = TestHelper.GetFileData(fileWithValues);
@@ -40,33 +199,50 @@ namespace ArtistStats.Test
             var releaseResults = deserializeJson.Deserialize(values);
             MusicStatService sut = new MusicStatService();
             MusicInterpretor sut2 = new MusicInterpretor(sut);
-            List<Release> enhancedReleases = new List<Release>();
+            List<Release> Releases = new List<Release>();
             for (int i = 0; i < releaseResults.Releases.Count; i++)
             {
-                CommandFactory commandFactory = new GetTracksByReleaseIDFactory(releaseResults.Releases[i].ID, sut);
+                CommandFactory commandFactory = new GetTracksByReleaseIDFactory(releaseResults.Releases[i], sut);
                 Command command = commandFactory.GetCommand();
                 Task t = command.ExecuteAsync();
-                t.Wait();
-                GetTracksByReleaseID getTracksByReleaseID = (GetTracksByReleaseID)command;//doesn't filter non-unique tracks
-                if (getTracksByReleaseID.Release == null)
-                    continue;
-                var uniqueTracks = getTracksByReleaseID.Release.Media[0].Tracks;
-                var duplicates = uniqueTracks.GroupBy(x => x.Title)
-                              .Where(g => g.Count() > 1)
-                              .Select(y => y)
-                              .ToList();
-                Assert.Empty(duplicates);
-                enhancedReleases.Add(getTracksByReleaseID.Release);
-                /*var temp = releaseResults.Releases[i].Media[0];
-                temp.TrackCount = uniqueTracks.Count;
-                temp.Tracks = new List<Track>(uniqueTracks);
-                releaseResults.Releases[i].Media[0] = temp;
-                enhancedReleases.Add(releaseResults.Releases[i]);*/
+                t.ContinueWith(task =>
+                {
+                    if (task.Status == TaskStatus.RanToCompletion)
+                    {
+                        GetTracksByReleaseID getTracksByReleaseID = (GetTracksByReleaseID)command;//doesn't filter non-unique tracks
+                        Releases.Add(getTracksByReleaseID.Release);
+                    }
+                    else
+                    {
+                        Assert.False(true);
+                    }
+                });
             }
-            var totalTracks = enhancedReleases.SelectMany(x => x.Media).SelectMany(x => x.Tracks).ToList();
+            var totalTracks = Releases.Where(x => (x != null && x.Media != null)).SelectMany(x => x.Media).SelectMany(x => x.Tracks).ToList();
 
-            Assert.Equal(25, enhancedReleases.Count);
+            Assert.Equal(13, Releases.Count);
+            Assert.True(totalTracks.Count > 0);//why are we gettin random tracks back?
 
+        }
+        [Fact]
+        public void SelectsValidReleasesWithTracksWithMultiThread()
+        {
+
+            MusicStatService sut = new MusicStatService();
+            MusicInterpretor sut2 = new MusicInterpretor(sut);
+            List<Release> Releases = new List<Release>();
+            /*
+                        CommandFactory commandFactory = new GetReleasesByArtistsIDFactory("382f1005-e9ab-4684-afd4-0bdae4ee37f2", sut);
+                        Command command = commandFactory.GetCommand();
+                        command.ExecuteAsync().Wait();
+                        GetReleasesByArtistsID getTracksByReleaseID = (GetReleasesByArtistsID)command;//doesn't filter non-unique tracks
+                        Releases = getTracksByReleaseID.ReleaseResults.Releases;
+
+                        var totalTracks = Releases.Where(x => (x != null && x.Media != null)).SelectMany(x => x.Media).SelectMany(x => x.Tracks).ToList();
+
+                        Assert.Equal(14, Releases.Count);
+                        Assert.True(totalTracks.Count > 0);//why are we gettin random tracks back?
+            */
         }
     }
 
